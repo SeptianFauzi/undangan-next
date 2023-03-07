237a98fd0c43717f5f6b108e7702a72ccdaee6db
Septian <fauzy434@gmail.com>
2023-03-07 14:25:59 +0700

    fix copy button on tambah page

--- src/pages/tambah/index.js
+++ src/pages/tambah/index.js
@@ -47,8 +47,7 @@ export default function Tambah() {
                         {url && <>
                             <div className="d-lg-flex flex-sm-column justify-content-between align-items-center">
                                 <p className='m-0'>URL : {url}</p>
-                                <button onClick={() => navigator.clipboard.writeText(url).then(() => alert("Berhasil Di Copy"))} class="btn btn-primary text-white me-3"><i className="fa fa-copy"></i></button>
-                                <a href={`whatsapp://send?text=` + encodeURIComponent(`
+                                <button onClick={() => navigator.clipboard.writeText(`
 السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
 
 Dengan memohon rahmat Allah SWT, perkenankan kami mengundang Bapak / Ibu / Saudara / i  untuk menghadiri pernikahan kami.
@@ -75,13 +74,41 @@ Merupakan suatu kebahagiaan bagi kami apabila Bapak /Ibu/Saudara/i berkenan untu
 
 Terima kasih banyak atas perhatiannya
 
+وَ السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ`).then(() => alert("Berhasil Di Copy"))} class="btn btn-primary text-white me-3"><i className="fa fa-copy"></i></button>
+                                <a href={`whatsapp://send?text=` + encodeURIComponent(`
+                                السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
+
+                                Dengan memohon rahmat Allah SWT, perkenankan kami mengundang Bapak / Ibu / Saudara / i  untuk menghadiri pernikahan kami.
+
+                                *Siti Nanda Nurdiani & Septian Fauzi*
+
+                                *Akad*
+                                Akan dilaksanakan pada :
+                                Hari, Tanggal : Minggu, 12 Maret 2023
+                                Jam : 09:00
+                                Lokasi : Kp. Sirnasari RT. 003 RW. 002 Desa Gunung Sari Kecamatan Pamijahan Kabupaten Bogor
+
+                                *Resepsi*
+                                Akan dilaksanakan pada :
+                                Hari, Tanggal : Minggu, 12 Maret 2023
+                                Jam : 13:00 s/d selesai
+                                Lokasi : Kp. Sirnasari RT. 003 RW. 002 Desa Gunung Sari Kecamatan Pamijahan Kabupaten Bogor
+
+                                Berikut link undangan kami, untuk info lengkap dari acara bisa kunjungi :
+
+                                ${url}
+
+                                Merupakan suatu kebahagiaan bagi kami apabila Bapak /Ibu/Saudara/i berkenan untuk hadir dan memberikan doa restu.
+
+                                Terima kasih banyak atas perhatiannya
+
 وَ السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ`)}><i class="fa-solid fa-paper-plane " aria-hidden="true"></i></a>
                             </div>
                         </>}
                     </div>
 
                 </div>
-            </div>
+            </div >
         </>
     )
 }
