01ad9dbb50d9ff2ba6d340df9e65ec536f587d0d
Septian <fauzy434@gmail.com>
2023-03-07 14:22:12 +0700

    fix copy button on daftar page

--- src/pages/daftar/index.js
+++ src/pages/daftar/index.js
@@ -48,7 +48,34 @@ export default function Daftar() {
                                     <td>{item.Nama}</td>
                                     <td>https://undangan.septianfauzi.com/{item.ID}</td>
                                     <td className='d-flex gap-3 align-items-center'>
-                                        <button onClick={() => navigator.clipboard.writeText("http://undangan.septianfauzi.com/" + item.ID).then(() => alert("Berhasil Di Copy"))} class="btn btn-primary text-white"><i className="fa fa-copy"></i></button>
+                                        <button onClick={() => navigator.clipboard.writeText(`
+السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
+
+Dengan memohon rahmat Allah SWT, perkenankan kami mengundang Bapak / Ibu / Saudara / i  untuk menghadiri pernikahan kami.
+                                        
+*Siti Nanda Nurdiani & Septian Fauzi*
+                                        
+*Akad*
+Akan dilaksanakan pada :
+Hari, Tanggal : Minggu, 12 Maret 2023
+Jam : 09:00
+Lokasi : Kp. Sirnasari RT. 003 RW. 002 Desa Gunung Sari Kecamatan Pamijahan Kabupaten Bogor
+
+*Resepsi*
+Akan dilaksanakan pada :
+Hari, Tanggal : Minggu, 12 Maret 2023
+Jam : 13:00 s/d selesai
+Lokasi : Kp. Sirnasari RT. 003 RW. 002 Desa Gunung Sari Kecamatan Pamijahan Kabupaten Bogor
+
+Berikut link undangan kami, untuk info lengkap dari acara bisa kunjungi :
+
+${`https://undangan.septianfauzi.com/` + item.ID}
+
+Merupakan suatu kebahagiaan bagi kami apabila Bapak /Ibu/Saudara/i berkenan untuk hadir dan memberikan doa restu.
+
+Terima kasih banyak atas perhatiannya
+
+وَ السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ` + item.ID).then(() => alert("Berhasil Di Copy"))} class="btn btn-primary text-white"><i className="fa fa-copy"></i></button>
                                         <a href={`whatsapp://send?text=` + encodeURIComponent(`
 السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ
 
